f878eff0304659f1741cc6cc71b7d51dcf5e0216
huttongrabiel <huttonthomas@icloud.com>
2022-05-23 21:01:13 -0700

    Main: Setup main for file reading and writing
    Get file from second CLI argument. Write quick error messages for not
    providing a source code file and for if the file cannot be found. Open
    and close file, works as expected.

--- projects/07/VMTranslator/Main.cpp
+++ projects/07/VMTranslator/Main.cpp
@@ -1,3 +1,35 @@
 /*
  *  Copyright 2022, Hutton Grabiel
  */
+
+#include <Lexer.h>
+#include <Parser.h>
+#include <Codegen.h>
+#include <fstream>
+#include <iostream>
+
+void output_hack_asm(char const* file_name) {
+    // Open file for writing, translate file, close file 
+
+    std::fstream output_file(file_name);
+    if (!output_file.is_open()) {
+        std::cerr << "\033[01;31mError: \033[0mFile '" << file_name << "' not found!" << std::endl;
+        exit(1);
+    }
+
+    output_file.close();
+}
+
+int main(int argc, char* argv[]) {
+    // Open file provided in CLI args, pass to output_hack_asm, close file
+
+    if (argc < 2) {
+        std::cerr << "\033[01;31mError: \033[0mPlease provide source file." << std::endl;
+        exit(1);
+    }
+
+    output_hack_asm(argv[1]);
+
+    return 0;
+}
+
