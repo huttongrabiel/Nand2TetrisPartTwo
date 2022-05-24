31ff4139c49024f6a3fa70481d5708242d79cab9
huttongrabiel <huttonthomas@icloud.com>
2022-05-23 23:33:02 -0700

    Parser: Parse lexed line
    Parser::parse(std::vector<std::string> tokens) returns a vector of
    CommandType enumerations. These command type enumerations will be
    switched in Codegen and generate the correct code based on what they
    say the structure of the instruction is.
    
    Example:
        If we have instruction 'push local 2', parser will return
        [CommandType::MemoryAccess, CommandType::Push, CommandType::Local].
        Should the CommandType::Local be omitted? MemoryAccess and Push
        might be enough information. Will consider tomorrow...

--- projects/07/VMTranslator/Parser.cpp
+++ projects/07/VMTranslator/Parser.cpp
@@ -2,3 +2,54 @@
  *  Copyright 2022, Hutton Grabiel
  */
 
+#include <Parser.h>
+
+// Not liking this at all, will see how it works. Is this better than 
+// a bunch of if - else if statements? Sure is an eyesore that can't be
+// hidden away in a class...
+std::map<std::string, Parser::CommandType> Parser::command_enum_map  = {
+            {"add",      Parser::CommandType::Add},
+            {"sub",      Parser::CommandType::Subtract},
+            {"neg",      Parser::CommandType::Negative},
+            {"eq",       Parser::CommandType::Equals},
+            {"gt",       Parser::CommandType::GreaterThan},
+            {"lt",       Parser::CommandType::LessThan},
+            {"and",      Parser::CommandType::And},
+            {"or",       Parser::CommandType::Or},
+            {"not",      Parser::CommandType::Not},
+            {"push",     Parser::CommandType::Push},
+            {"pop",      Parser::CommandType::Pop},
+            {"constant", Parser::CommandType::Constant},
+            {"local",    Parser::CommandType::Local},
+            {"static",   Parser::CommandType::Static},
+            {"this",     Parser::CommandType::This},
+            {"that",     Parser::CommandType::That}
+};
+
+std::vector<Parser::CommandType> Parser::parse(std::vector<std::string> const& tokens)
+{
+    std::vector<Parser::CommandType> parsed_instruction;
+
+    // token can equal push, pop, add, sub, neg, eq, gt, lt, and, or, not, constant, local, static, this, that, offset
+
+    if (tokens.size() == 1)
+        parsed_instruction.push_back(CommandType::Arithmetic);
+    else
+        parsed_instruction.push_back(CommandType::MemoryAccess);
+
+    if (parsed_instruction[0] == CommandType::Arithmetic) {
+        auto command = Parser::command_enum_map.find(tokens[0]);
+        parsed_instruction.push_back(command->second);
+    }
+    else if (parsed_instruction[0] == CommandType::MemoryAccess) {
+        auto push_or_pop = Parser::command_enum_map.find(tokens[0]);
+        parsed_instruction.push_back(push_or_pop->second);
+
+        auto memory_segment = Parser::command_enum_map.find(tokens[1]);
+        parsed_instruction.push_back(memory_segment->second);
+        
+        parsed_instruction.push_back(CommandType::Offset);
+    }
+
+    return parsed_instruction;
+}
--- projects/07/VMTranslator/Parser.h
+++ projects/07/VMTranslator/Parser.h
@@ -3,3 +3,37 @@
  */
 
 #pragma once
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+
+class Parser {
+public:
+    enum class CommandType {
+        MemoryAccess,
+        Arithmetic,
+        Push,
+        Pop,
+        Add,
+        Subtract,
+        Negative,
+        Equals,
+        GreaterThan,
+        LessThan,
+        And,
+        Or,
+        Not,
+        Constant,
+        Local,
+        Static,
+        This,
+        That,
+        Offset
+    };
+
+    static std::vector<CommandType> parse(std::vector<std::string> const&);
+private:
+    static std::map<std::string, CommandType> command_enum_map;
+};
