d7d982346cf327c77afe9923459721b19a121743
huttongrabiel <huttonthomas@icloud.com>
2022-05-24 21:36:45 -0700

    Codegen: Add to_int(std::string) helper
    Convert int to string.

--- projects/07/VMTranslator/Codegen.h
+++ projects/07/VMTranslator/Codegen.h
@@ -14,4 +14,14 @@ public:
     static std::string generate_hack_asm(std::vector<Parser::CommandType> const&, std::vector<std::string> const&, std::string const&, std::string const&);
 private:
     static std::string label_name(Parser::CommandType const&, std::string const&, std::vector<std::string> const&);
+    static int to_int(std::string str) 
+    {
+        int res = 0;
+        int mult = 1;
+        for (int i = str.size()-1; i >= 0; i--) {
+            res += mult * (str[i] - '0');
+            mult *= 10;
+        }
+        return res;
+    } 
 };
