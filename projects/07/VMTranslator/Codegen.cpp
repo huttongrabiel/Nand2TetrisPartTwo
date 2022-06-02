436d344baa95fbdb2ff8c7c3ae2e9d2fdc9787ff
huttongrabiel <huttonthomas@icloud.com>
2022-06-01 20:54:05 -0700

    Codegen: Revert earlier commit about checking for temp
    Temp does not need to be handled differently. We just use the same base
    address everytime so we don't need to modify it.

--- projects/07/VMTranslator/Codegen.cpp
+++ projects/07/VMTranslator/Codegen.cpp
@@ -216,7 +216,7 @@ std::string Codegen::generate_pop_assembly(Parser::CommandType const& next_instr
     output.append("D=A\n");
     output.append(label_name(next_instruction, source_code_file_name, offset_token));
 
-    if (next_instruction == Parser::CommandType::Temp || next_instruction == Parser::CommandType::Static)
+    if (next_instruction == Parser::CommandType::Temp)
         output.append("D=D+A\n");
     else if (next_instruction == Parser::CommandType::Pointer)
         output.append("D=A\n");
